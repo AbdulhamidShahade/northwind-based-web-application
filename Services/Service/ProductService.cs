d48fa4f18cf4be624b0a4297776640ec85860f0b
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:28:42 +0300

    Add ProductService.cs

---
+++ Services/Service/ProductService.cs
@@ -0,0 +1,76 @@
+﻿using FullWebProjectWithAPI.Web.Models.DTOs.ProductDTOs;
+using FullWebProjectWithAPI.Web.Services.Base;
+using FullWebProjectWithAPI.Web.Services.IService;
+using System.Collections.Concurrent;
+using System.Linq.Expressions;
+
+namespace FullWebProjectWithAPI.Web.Services.Service
+{
+    public class ProductService : BaseService, IProductService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly string _url;
+
+        public ProductService(IHttpClientFactory httpClientFactory, 
+            IConfiguration configuration) : base(httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            _url = configuration.GetValue<string>("ApiUrls:Base");
+        }
+
+        public Task<T> CreateAsync<T>(CreateProductDto createDto, string token)
+        {
+            return SendAsync<T>(new Models.ApiRequest
+            {
+                ApiType = Models.Enums.ApiType.GET,
+                Data = createDto,
+                Token = token,
+                ApiUrl = $"{_url}/product"
+            });
+        }
+
+        public Task<T> DeleteAsync<T>(Func<int, int> func, string token)
+        {
+            return SendAsync<T>(new Models.ApiRequest
+            {
+                ApiType = Models.Enums.ApiType.DELETE,
+                Data = null,
+                Token = token,
+                ApiUrl = $"{_url}/product/{func(0)}"
+            });
+        }
+
+        public Task<T> GetAllAsync<T>(string token)
+        {
+            return SendAsync<T>(new Models.ApiRequest
+            {
+                ApiType = Models.Enums.ApiType.GET,
+                Data = null,
+                Token = token,
+                ApiUrl = $"{_url}/product"
+            });
+        }
+
+        public Task<T> GetAsync<T>(Func<int, int> func, string token)
+        {
+            return SendAsync<T>(new Models.ApiRequest
+            {
+                ApiType = Models.Enums.ApiType.GET,
+                Data = null,
+                Token = token,
+                ApiUrl = $"{_url}/product/{func(0)}"
+            });
+        }
+
+        public Task<T> UpdateAsync<T>(Func<int, int> func, UpdateProductDto updateDto, string token)
+        {
+            return SendAsync<T>(new Models.ApiRequest
+            {
+                ApiType = Models.Enums.ApiType.PUT,
+                Data = updateDto,
+                Token = token,
+                ApiUrl = $"{_url}/product/{func(0)}"
+            });
+        }
+    }
+}
