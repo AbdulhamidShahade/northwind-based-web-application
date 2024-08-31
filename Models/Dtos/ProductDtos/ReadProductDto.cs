a061ba10c1a39889b4bd9a7791c713800fb58d16
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:21:19 +0300

    Add product dtos

---
+++ Models/Dtos/ProductDtos/ReadProductDto.cs
@@ -0,0 +1,82 @@
+﻿using NorthwindBasedWebApplication.Models.Dtos.CategoryDtos;
+using NorthwindBasedWebApplication.Models.Dtos.OrderDetailsDto;
+using NorthwindBasedWebApplication.Models.Dtos.SupplierDtos;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace NorthwindBasedWebApplication.Models.Dtos.ProductDtos
+{
+    public class ReadProductDto
+    {
+
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+
+
+
+        
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+
+
+
+       
+        [Display(Name = "Quantity Per Unit")]
+        public string QuantityPerUnit { get; set; }
+
+
+
+        
+        [Display(Name = "Unit Price")]
+        public decimal UnitPrice { get; set; }
+
+
+
+        
+        [Display(Name = "Units In Stock")]
+        public short UnitsInStock { get; set; }
+
+
+
+        
+        [Display(Name = "Units On Order")]
+        public short UnitsOnOrder { get; set; }
+
+
+
+       
+        [Display(Name = "Reorder Level")]
+        public short ReorderLevel { get; set; }
+
+
+
+        
+        [Required(ErrorMessage = "Discontinued is required field!")]
+        public bool Discontinued { get; set; }
+
+
+        [Display(Name = "Picture")]
+        public string? PictureUrl { get; set; }
+
+
+
+        //Navigation properties
+        
+        [Display(Name = "Category Id")]
+
+        public int CategoryId { get; set; }
+        public ReadCategoryDto CategoryDto { get; set; }
+
+
+
+
+        [Display(Name = "Supplier Id")]
+
+        public int SupplierId { get; set; }
+        public ReadSupplierDto SupplierDto { get; set; }
+
+
+        public ICollection<ReadOrderDetailsDto> OrderDetailsDto { get; set; }
+    }
+}
