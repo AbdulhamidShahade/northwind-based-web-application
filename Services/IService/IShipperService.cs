d5333dcd038d1503527cf2646ea8026881f3270a
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:27:20 +0300

    Add IShipperService.cs

---
+++ Services/IService/IShipperService.cs
@@ -0,0 +1,16 @@
+﻿using FullWebProjectWithAPI.Web.Models.DTOs.CategoryDTOs;
+using FullWebProjectWithAPI.Web.Models.DTOs.ProductDTOs;
+using FullWebProjectWithAPI.Web.Models.DTOs.ShipperDTOs;
+using System.Linq.Expressions;
+
+namespace FullWebProjectWithAPI.Web.Services.IService
+{
+    public interface IShipperService
+    {
+        Task<T> GetAllAsync<T>(string token);
+        Task<T> GetAsync<T>(Func<int, int> func, string token);
+        Task<T> CreateAsync<T>(CreateShipperDto createDto, string token);
+        Task<T> UpdateAsync<T>(Func<int, int> func, UpdateShipperDto updateDto, string token);
+        Task<T> DeleteAsync<T>(Func<int, int> func, string token);
+    }
+}
