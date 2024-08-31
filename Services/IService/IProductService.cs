1e4175aa1d371b57e9ec26f55d4fb56e30431d4a
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:27:02 +0300

    Add IProductService.cs

---
+++ Services/IService/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using FullWebProjectWithAPI.Web.Models.DTOs.CategoryDTOs;
+using FullWebProjectWithAPI.Web.Models.DTOs.ProductDTOs;
+using System.Linq.Expressions;
+
+namespace FullWebProjectWithAPI.Web.Services.IService
+{
+    public interface IProductService
+    {
+        Task<T> GetAllAsync<T>(string token);
+        Task<T> GetAsync<T>(Func<int, int> func, string token);
+        Task<T> CreateAsync<T>(CreateProductDto createDto, string token);
+        Task<T> UpdateAsync<T>(Func<int, int> func, UpdateProductDto updateDto, string token);
+        Task<T> DeleteAsync<T>(Func<int, int> func, string token);
+    }
+}
