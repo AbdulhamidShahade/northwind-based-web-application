0aeaeca4137515d1d24dddc49171260eeddce456
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:21:02 +0300

    Add shipper dtos

---
+++ Models/Dtos/ShipperDtos/ReadShipperDto.cs
@@ -0,0 +1,34 @@
+﻿
+using NorthwindBasedWebApplication.Models.Dtos.OrderDtos;
+using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebApplication.Models.Dtos.ShipperDtos
+{
+    public class ReadShipperDto
+    {
+
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+
+
+        [Display(Name = "Company Name")]
+        public string CompanyName { get; set; }
+
+
+
+
+        [Display(Name = "Phone")]
+        public string Phone { get; set; }
+
+
+
+        [Display(Name = "Picture")]
+        public string? PictureUrl { get; set; }
+
+
+
+        //Navigation Properties
+        public ICollection<ReadOrderDto> OrdersDto { get; set; }
+    }
+}
