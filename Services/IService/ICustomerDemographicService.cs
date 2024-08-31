5ad77e7c87fade3327493d76fb59d020136d9f4f
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:26:22 +0300

    Add ICustomerDemographicService

---
+++ Services/IService/ICustomerDemographicService.cs
@@ -0,0 +1,16 @@
+﻿using FullWebProjectWithAPI.Web.Models.DTOs.CustomerDemographicDTOs;
+using NorthwindTask.API.Models.DTOs.CustomerCustomerDemographicDTOs;
+using System.Linq.Expressions;
+
+namespace FullWebProjectWithAPI.Web.Services.IService
+{
+    public interface ICustomerDemographicService
+    { 
+        Task<T> GetAllAsync<T>(string token);
+        Task<T> GetAsync<T>(Func<int, int> func, string token);
+        Task<T> CreateAsync<T>(CreateCustomerDemographicDto createDto, string token);
+        Task<T> UpdateAsync<T>(Func<int, int> func,
+            UpdateCustomerDemographicDto updateDto, string token);
+        Task<T> DeleteAsync<T>(Func<int, int> func, string token);
+    }
+}
