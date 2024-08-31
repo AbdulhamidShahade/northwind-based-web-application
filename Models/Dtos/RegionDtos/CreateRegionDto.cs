38b1d4d0f43663554b3cd4f45b108331534ffdd2
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:20:55 +0300

    Add region dtos

---
+++ Models/Dtos/RegionDtos/CreateRegionDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebApplication.Models.Dtos.RegionDtos
+{
+    public class CreateRegionDto
+    {
+
+        [Required(ErrorMessage = "Region description is required!")]
+        [Display(Name = "Region Description")]
+        public string RegionDescription { get; set; }
+
+
+        [Display(Name = "Picture")]
+        public string? PictureUrl { get; set; }
+
+    }
+}
---
+++ Models/Dtos/RegionDtos/ReadRegionDto.cs
@@ -0,0 +1,29 @@
+﻿using FullWebProjectWith
+
+using NorthwindBasedWebApplication.Models.Dtos.TerritoryDtos;
+using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebApplication.Models.Dtos.RegionDtos
+{
+    public class ReadRegionDto
+    {
+
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+
+
+        [Display(Name = "Region Description")]
+        public string RegionDescription { get; set; }
+
+
+        [Display(Name = "Picture")]
+        public string? PictureUrl { get; set; }
+
+
+
+        //Navigation Properties
+        public ICollection<ReadTerritoryDto> TerritoriesDto { get; set; }
+
+    }
+}
