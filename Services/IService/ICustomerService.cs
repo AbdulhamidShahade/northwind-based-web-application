4f2cfee0d1b5195090627318a850c85f2bbcc4a6
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:26:34 +0300

    Add ICustomerService.cs

---
+++ Services/IService/ICustomerService.cs
@@ -0,0 +1,14 @@
+﻿using FullWebProjectWithAPI.Web.Models.DTOs.CustomerDTOs;
+using System.Linq.Expressions;
+
+namespace FullWebProjectWithAPI.Web.Services.IService
+{
+    public interface ICustomerService
+    {
+        Task<T> GetAllAsync<T>(string token);
+        Task<T> GetAsync<T>(Func<int, int> func, string token);
+        Task<T> CreateAsync<T>(CreateCustomerDto createDto, string token);
+        Task<T> UpdateAsync<T>(Func<int, int> func, UpdateCustomerDto updateDto, string token);
+        Task<T> DeleteAsync<T>(Func<int, int> func, string token);
+    }
+}
