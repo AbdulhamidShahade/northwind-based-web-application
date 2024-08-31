39125655ffa4b94ac67e66485e5cc1a394a5ff30
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:20:00 +0300

    Add CustomerCustomerDemographic dtos

---
+++ Models/Dtos/CustomerCustomerDemographicDtos/ReadCustomerCustomerDemographicDto.cs
@@ -0,0 +1,25 @@
+﻿
+using NorthwindBasedWebApplication.Models.Dtos.CustomerDemographicDtos;
+using NorthwindBasedWebApplication.Models.Dtos.CustomerDtos;
+using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebApplication.Models.Dtos.CustomerCustomerDemographicDtos
+{
+    public class ReadCustomerCustomerDemographicDto
+    {
+
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+
+        [Display(Name = "Customer Id")]
+        public int CustomerId { get; set; }
+        public ReadCustomerDto CustomerDto { get; set; }
+
+
+
+        [Display(Name = "Customer Demographic")]
+        public int CustomerDemographicId { get; set; }
+        public ReadCustomerDemographicDto CustomerDemographicDto { get; set; }
+    }
+}
