a99ea87f084195cf35cafd4b4523fec3d6835e92
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:22:00 +0300

    Add order details dtos

---
+++ Models/Dtos/OrderDetailsDtos/ReadOrderDetailsDto.cs
@@ -0,0 +1,38 @@
+﻿using NorthwindBasedWebApplication.Models.Dtos.OrderDtos;
+using NorthwindBasedWebApplication.Models.Dtos.ProductDtos;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace NorthwindBasedWebApplication.Models.Dtos.OrderDetailsDto
+{
+    public class ReadOrderDetailsDto
+    {
+
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+
+        [Display(Name = "Unit Price")]
+        public decimal UnitPrice { get; set; }
+
+
+        [Display(Name = "Quantity")]
+        public short Quantity { get; set; }
+
+
+
+        [Display(Name = "Discount")]
+        public float Discount { get; set; }
+
+
+        //Navigation Properties 
+        [Display(Name = "Order Id")]
+        public int OrderId { get; set; }
+        public ReadOrderDto OrderDto { get; set; }
+
+
+        [Display(Name = "Product Id")]
+        public int ProductId { get; set; }
+        public ReadProductDto ProductDto { get; set; }
+    }
+}
