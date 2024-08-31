8ab75f23c76885f9b55472a28ba31c90272b15e4
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:26:43 +0300

    Add IEmployeeService.cs

---
+++ Services/IService/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using FullWebProjectWithAPI.Web.Models.DTOs.EmployeeDTOs;
+using System.Linq.Expressions;
+
+namespace FullWebProjectWithAPI.Web.Services.IService
+{
+    public interface IEmployeeService
+    {
+        Task<T> GetAllAsync<T>(string token);
+        Task<T> GetAsync<T>(Func<int, int> func, string token);
+        Task<T> CreateAsync<T>(CreateEmployeeDto createDto, string token);
+        Task<T> UpdateAsync<T>(Func<int, int> func, UpdateEmployeeDto updateDto, string token);
+        Task<T> DeleteAsync<T>(Func<int, int> func, string token);
+    }
+}
