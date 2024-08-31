2f6c922bb5eeb92bce4ee6c10800bd722f74bff0
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:26:51 +0300

    Add IOrderService.cs

---
+++ Services/IService/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using FullWebProjectWithAPI.Web.Models.DTOs.OrderDTOs;
+using System.Linq.Expressions;
+
+namespace FullWebProjectWithAPI.Web.Services.IService
+{
+    public interface IOrderService
+    {
+        Task<T> GetAllAsync<T>(string token);
+        Task<T> GetAsync<T>(Func<int, int> func, string token);
+        Task<T> CreateAsync<T>(CreateOrderDto createDto, string token);
+        Task<T> UpdateAsync<T>(Func<int, int> func, UpdateOrderDto updateDto, string token);
+        Task<T> DeleteAsync<T>(Func<int, int> func, string token);
+    }
+}
