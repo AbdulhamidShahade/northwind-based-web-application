a1284bec48570661caf5c7f107e47c5c2840a120
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:21:29 +0300

    Add territory dtos

---
+++ Models/Dtos/TerritoryDtos/ReadTerritoryDto.cs
@@ -0,0 +1,34 @@
+﻿
+using NorthwindBasedWebApplication.Models.Dtos.EmployeeTerritoryDtos;
+using NorthwindBasedWebApplication.Models.Dtos.RegionDtos;
+using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebApplication.Models.Dtos.TerritoryDtos
+{
+    public class ReadTerritoryDto
+    {
+
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+
+
+        [Display(Name = "Territory Description")]
+        public string TerritoryDescription { get; set; }
+
+
+
+        [Display(Name = "Picture")]
+        public string? PictureUrl { get; set; }
+
+
+
+        [Display(Name = "Region")]
+        public int RegionId { get; set; }
+        public ReadRegionDto RegionDto { get; set; }
+
+
+
+        public ICollection<ReadEmployeeTerritoryDto> EmployeesTerritoriesDto { get; set; }
+    }
+}
