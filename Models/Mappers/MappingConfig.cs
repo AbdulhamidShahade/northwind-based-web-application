f539851363a7547493809d18a1bc6b0f82d51550
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:25:31 +0300

    Add mapping config for dtos

---
+++ Models/Mappers/MappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FullWebProjectWithAPI.Web.Models.DTOs.CategoryDTOs;
+
+namespace FullWebProjectWithAPI.Web.Models.Mappers
+{
+    public class MappingConfig : Profile
+    {
+
+        public MappingConfig()
+        {
+           
+        }
+    }
+}
