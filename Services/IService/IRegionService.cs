3ea65adda4297b00b10819b1fee08766ee63af5c
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:27:10 +0300

    Add IRegionService.cs

---
+++ Services/IService/IRegionService.cs
@@ -0,0 +1,16 @@
+﻿using FullWebProjectWithAPI.Web.Models.DTOs.CategoryDTOs;
+using FullWebProjectWithAPI.Web.Models.DTOs.ProductDTOs;
+using FullWebProjectWithAPI.Web.Models.DTOs.RegionDTOs;
+using System.Linq.Expressions;
+
+namespace FullWebProjectWithAPI.Web.Services.IService
+{
+    public interface IRegionService
+    {
+        Task<T> GetAllAsync<T>(string token);
+        Task<T> GetAsync<T>(Func<int, int> func, string token);
+        Task<T> CreateAsync<T>(CreateRegionDto createDto, string token);
+        Task<T> UpdateAsync<T>(Func<int, int> func, UpdateRegionDto updateDto, string token);
+        Task<T> DeleteAsync<T>(Func<int, int> func, string token);
+    }
+}
