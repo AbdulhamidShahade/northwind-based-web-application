521a19b9d82e32e0c8f2344991f8ce73a7c460a4
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:21:49 +0300

    Add employee territory dtos

---
+++ Models/Dtos/EmployeeTerritoryDtos/ReadEmployeeTerritoryDto.cs
@@ -0,0 +1,26 @@
+﻿
+using NorthwindBasedWebApplication.Models.Dtos.EmployeeDtos;
+using NorthwindBasedWebApplication.Models.Dtos.TerritoryDtos;
+using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebApplication.Models.Dtos.EmployeeTerritoryDtos
+{
+    public class ReadEmployeeTerritoryDto
+    {
+
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+
+
+        [Display(Name = "Employee Id")]
+        public int EmployeeId { get; set; }
+        public ReadEmployeeDto EmployeeDto { get; set; }
+
+
+
+        [Display(Name = "Territory Id")]
+        public int TerritoryId { get; set; }
+        public ReadTerritoryDto TerritoryDto { get; set; }
+    }
+}
