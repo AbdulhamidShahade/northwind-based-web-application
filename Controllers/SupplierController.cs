547a307a940660d6b6854a3bef2f73adfa227479
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:32:17 +0300

    Add SupplierController.cs

---
+++ Controllers/SupplierController.cs
@@ -0,0 +1,128 @@
+﻿using AutoMapper;
+using FullWebProjectWithAPI.Web.Models.DTOs.CategoryDTOs;
+using FullWebProjectWithAPI.Web.Models.DTOs.CustomerDTOs;
+using FullWebProjectWithAPI.Web.Models;
+using FullWebProjectWithAPI.Web.Services.IService;
+using FullWebProjectWithAPI.Web.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using FullWebProjectWithAPI.Web.Models.DTOs.SupplierDTOs;
+
+namespace FullWebProjectWithAPI.Web.Controllers
+{
+    public class SupplierController : Controller
+    {
+        private readonly ISupplierService _supplierService;
+        private readonly IMapper _mapper;
+
+        public SupplierController(ISupplierService supplierService, IMapper mapper)
+        {
+            _supplierService = supplierService;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            var response = await _supplierService.GetAllAsync<ApiResponse>(HttpContext.Session.GetString(Shared.SessionToken));
+
+            var categories = JsonConvert.DeserializeObject<List<ReadSupplierDto>>(response.data.ToString());
+
+
+            return View(categories);
+
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Details(int id)
+        {
+            var response = await _supplierService.GetAsync<ApiResponse>(x => id, HttpContext.Session.GetString(Shared.SessionToken));
+
+            var category = JsonConvert.DeserializeObject<ReadSupplierDto>(response.data.ToString());
+
+            return View(category);
+        }
+
+
+        [HttpGet]
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateSupplierDto createSupplierDto)
+        {
+            var response = await _supplierService.CreateAsync<ApiResponse>(createSupplierDto, HttpContext.Session.GetString(Shared.SessionToken));
+
+            if (response.IsSuccess)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Update(int id)
+        {
+
+            var response = await _supplierService.GetAsync<ApiResponse>(x => id, HttpContext.Session.GetString(Shared.SessionToken));
+
+            var category = JsonConvert.DeserializeObject<UpdateSupplierDto>(response.data.ToString());
+
+            return View(category);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Update(int id, UpdateSupplierDto updateSupplierDto)
+        {
+            var response = await _supplierService.UpdateAsync<ApiResponse>(x => id, updateSupplierDto, HttpContext.Session.GetString(Shared.SessionToken));
+
+            if (response.IsSuccess)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var response = await _supplierService.GetAsync<ApiResponse>(x => id, HttpContext.Session.GetString(Shared.SessionToken));
+
+            var category = JsonConvert.DeserializeObject<ReadSupplierDto>(response.data.ToString());
+
+            return View(category);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(ReadCategoryDto model)
+        {
+            var response = await _supplierService.DeleteAsync<ApiResponse>(x => model.Id, HttpContext.Session.GetString(Shared.SessionToken));
+
+            if (response.IsSuccess && response != null)
+            {
+                return Ok("Ok");
+            }
+            else
+            {
+                return BadRequest("Failed");
+            }
+        }
+    }
+}
