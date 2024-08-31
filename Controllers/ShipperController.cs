2fb4038032d7036a9a71dacacb43acb19682ab9d
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:32:07 +0300

    Add ShipperController.cs

---
+++ Controllers/ShipperController.cs
@@ -0,0 +1,128 @@
+﻿using AutoMapper;
+using FullWebProjectWithAPI.Web.Models.DTOs.CategoryDTOs;
+using FullWebProjectWithAPI.Web.Models.DTOs.CustomerDTOs;
+using FullWebProjectWithAPI.Web.Models;
+using FullWebProjectWithAPI.Web.Services.IService;
+using FullWebProjectWithAPI.Web.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using FullWebProjectWithAPI.Web.Models.DTOs.ShipperDTOs;
+
+namespace FullWebProjectWithAPI.Web.Controllers
+{
+    public class ShipperController : Controller
+    {
+        private readonly IShipperService _shipperService;
+        private readonly IMapper _mapper;
+
+        public ShipperController(IShipperService shipperService, IMapper mapper)
+        {
+            _shipperService = shipperService;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            var response = await _shipperService.GetAllAsync<ApiResponse>(HttpContext.Session.GetString(Shared.SessionToken));
+
+            var categories = JsonConvert.DeserializeObject<List<ReadShipperDto>>(response.data.ToString());
+
+
+            return View(categories);
+
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Details(int id)
+        {
+            var response = await _shipperService.GetAsync<ApiResponse>(x => id, HttpContext.Session.GetString(Shared.SessionToken));
+
+            var category = JsonConvert.DeserializeObject<ReadCategoryDto>(response.data.ToString());
+
+            return View(category);
+        }
+
+
+        [HttpGet]
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateShipperDto createShipperDto)
+        {
+            var response = await _shipperService.CreateAsync<ApiResponse>(createShipperDto, HttpContext.Session.GetString(Shared.SessionToken));
+
+            if (response.IsSuccess)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Update(int id)
+        {
+
+            var response = await _shipperService.GetAsync<ApiResponse>(x => id, HttpContext.Session.GetString(Shared.SessionToken));
+
+            var category = JsonConvert.DeserializeObject<UpdateShipperDto>(response.data.ToString());
+
+            return View(category);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Update(int id, UpdateShipperDto updateShipperDto)
+        {
+            var response = await _shipperService.UpdateAsync<ApiResponse>(x => id, updateShipperDto, HttpContext.Session.GetString(Shared.SessionToken));
+
+            if (response.IsSuccess)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var response = await _shipperService.GetAsync<ApiResponse>(x => id, HttpContext.Session.GetString(Shared.SessionToken));
+
+            var category = JsonConvert.DeserializeObject<ReadShipperDto>(response.data.ToString());
+
+            return View(category);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(ReadCategoryDto model)
+        {
+            var response = await _shipperService.DeleteAsync<ApiResponse>(x => model.Id, HttpContext.Session.GetString(Shared.SessionToken));
+
+            if (response.IsSuccess && response != null)
+            {
+                return Ok("Ok");
+            }
+            else
+            {
+                return BadRequest("Failed");
+            }
+        }
+    }
+}
