be61ab02e4fd70433f1cf4bb85b38e06b77c61e8
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:28:50 +0300

    Add RegionService.cs

---
+++ Services/Service/RegionService.cs
@@ -0,0 +1,75 @@
+﻿using FullWebProjectWithAPI.Web.Models.DTOs.RegionDTOs;
+using FullWebProjectWithAPI.Web.Services.Base;
+using FullWebProjectWithAPI.Web.Services.IService;
+using System.Linq.Expressions;
+
+namespace FullWebProjectWithAPI.Web.Services.Service
+{
+    public class RegionService : BaseService, IRegionService
+    {
+        private readonly IHttpClientFactory _httpClientFactor;
+        private readonly string _url;
+
+        public RegionService(IHttpClientFactory httpClientFactory,
+            IConfiguration configuration) : base(httpClientFactory)
+        {
+            _httpClientFactor = httpClientFactory;
+            _url = configuration.GetValue<string>("ApiUrls:Base");
+        }
+
+        public Task<T> CreateAsync<T>(CreateRegionDto createDto, string token)
+        {
+            return SendAsync<T>(new Models.ApiRequest
+            {
+                ApiType = Models.Enums.ApiType.GET,
+                Data = createDto,
+                Token = token,
+                ApiUrl = $"{_url}/region"
+            });
+        }
+
+        public Task<T> DeleteAsync<T>(Func<int, int> func, string token)
+        {
+            return SendAsync<T>(new Models.ApiRequest
+            {
+                ApiType = Models.Enums.ApiType.DELETE,
+                Data = null,
+                Token = token,
+                ApiUrl = $"{_url}/region/{func(0)}"
+            });
+        }
+
+        public Task<T> GetAllAsync<T>(string token)
+        {
+            return SendAsync<T>(new Models.ApiRequest
+            {
+                ApiType = Models.Enums.ApiType.GET,
+                Data = null,
+                Token = token,
+                ApiUrl = $"{_url}/region"
+            });
+        }
+
+        public Task<T> GetAsync<T>(Func<int, int> func, string token)
+        {
+            return SendAsync<T>(new Models.ApiRequest
+            {
+                ApiType = Models.Enums.ApiType.GET,
+                Data = null,
+                Token = token,
+                ApiUrl = $"{_url}/region/{func(0)}"
+            });
+        }
+
+        public Task<T> UpdateAsync<T>(Func<int, int> func, UpdateRegionDto updateDto, string token)
+        {
+            return SendAsync<T>(new Models.ApiRequest
+            {
+                ApiType = Models.Enums.ApiType.PUT,
+                Data = updateDto,
+                Token = token,
+                ApiUrl = $"{_url}/region/{func(0)}"
+            });
+        }
+    }
+}
