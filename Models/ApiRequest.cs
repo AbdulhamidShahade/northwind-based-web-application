280fbf12cef73bf5341922ef38a492ed42350996
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:24:50 +0300

    Add api request model

---
+++ Models/ApiRequest.cs
@@ -0,0 +1,12 @@
+﻿using FullWebProjectWithAPI.Web.Models.Enums;
+
+namespace FullWebProjectWithAPI.Web.Models
+{
+    public class ApiRequest
+    {
+        public ApiType ApiType { get; set; }
+        public string ApiUrl { get; set; }
+        public object Data { get; set; }
+        public string Token { get; set; }
+    }
+}
