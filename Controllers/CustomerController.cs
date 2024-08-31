083e8df4376892e08360011b7607c7a2f3c58171
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:31:06 +0300

    Add CustomerController.cs

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,139 @@
+﻿using AutoMapper;
+using FullWebProjectWithAPI.Web.Models;
+using FullWebProjectWithAPI.Web.Models.DTOs.CategoryDTOs;
+using FullWebProjectWithAPI.Web.Models.DTOs.CustomerDTOs;
+using FullWebProjectWithAPI.Web.Services.IService;
+using FullWebProjectWithAPI.Web.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FullWebProjectWithAPI.Web.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        private readonly ICustomerService _customerService;
+        private readonly IMapper _mapper;
+
+        public CustomerController(ICustomerService customerService, IMapper mapper)
+        {
+            _customerService = customerService;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LoadData()
+        {
+
+            
+            var response = await _customerService.GetAllAsync<ApiResponse>(HttpContext.Session.GetString(Shared.SessionToken));
+            if (response == null || !response.IsSuccess)
+            {
+                return Json(new { data = new List<object>() });
+            }
+
+            var test = JsonConvert.DeserializeObject < List <custom>> (Convert.ToString(response.data));
+
+            return Json(new { data = test });
+
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Details(int id)
+        {
+            var response = await _customerService.GetAsync<ApiResponse>(x => id, HttpContext.Session.GetString(Shared.SessionToken));
+
+            var category = JsonConvert.DeserializeObject<ReadCustomerDto>(response.data.ToString());
+
+            return View(category);
+        }
+
+
+        [HttpGet]
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateCustomerDto createCustomerDto)
+        {
+            var response = await _customerService.CreateAsync<ApiResponse>(createCustomerDto, HttpContext.Session.GetString(Shared.SessionToken));
+
+            if (response.IsSuccess)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Update(int id)
+        {
+
+            var response = await _customerService.GetAsync<ApiResponse>(x => id, HttpContext.Session.GetString(Shared.SessionToken));
+
+            var category = JsonConvert.DeserializeObject<UpdateCustomerDto>(response.data.ToString());
+
+            return View(category);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Update(int id, UpdateCustomerDto updateCustomerDto)
+        {
+            var response = await _customerService.UpdateAsync<ApiResponse>(x => id, updateCustomerDto, HttpContext.Session.GetString(Shared.SessionToken));
+
+            if (response.IsSuccess)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var response = await _customerService.GetAsync<ApiResponse>(x => id, HttpContext.Session.GetString(Shared.SessionToken));
+
+            var category = JsonConvert.DeserializeObject<ReadCustomerDto>(response.data.ToString());
+
+            return View(category);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(ReadCustomerDto model)
+        {
+            var response = await _customerService.DeleteAsync<ApiResponse>(x => model.Id, HttpContext.Session.GetString(Shared.SessionToken));
+
+            if (response.IsSuccess && response != null)
+            {
+                return Ok("Ok");
+            }
+            else
+            {
+                return BadRequest("Failed");
+            }
+        }
+    }
+}
