7dcb4fce6b86c04f20bb5550b447647ad786c30d
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:25:51 +0300

    Add IBaseService

---
+++ Services/IBase/IBaseService.cs
@@ -0,0 +1,12 @@
+﻿using FullWebProjectWithAPI.Web.Models;
+using System.Linq.Expressions;
+
+namespace FullWebProjectWithAPI.Web.Services.IBase
+{
+    public interface IBaseService
+    {
+        ApiResponse Response { get; set; }
+
+        Task<T> SendAsync<T>(ApiRequest request);
+    }
+}
