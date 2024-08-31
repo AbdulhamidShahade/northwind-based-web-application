1918db97d4a50c1bca6ce010bb7ab2285496a15d
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:32:26 +0300

    Add TerritoryController.cs

---
+++ Controllers/TerritoryController.cs
@@ -0,0 +1,128 @@
+﻿using AutoMapper;
+using FullWebProjectWithAPI.Web.Models.DTOs.CategoryDTOs;
+using FullWebProjectWithAPI.Web.Models.DTOs.CustomerDTOs;
+using FullWebProjectWithAPI.Web.Models;
+using FullWebProjectWithAPI.Web.Services.IService;
+using FullWebProjectWithAPI.Web.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using FullWebProjectWithAPI.Web.Models.DTOs.TerritoryDTOs;
+
+namespace FullWebProjectWithAPI.Web.Controllers
+{
+    public class TerritoryController : Controller
+    {
+        private readonly ITerritoryService _territoryService;
+        private readonly IMapper _mapper;
+
+        public TerritoryController(ITerritoryService territoryService, IMapper mapper)
+        {
+            _territoryService = territoryService;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            var response = await _territoryService.GetAllAsync<ApiResponse>(HttpContext.Session.GetString(Shared.SessionToken));
+
+            var categories = JsonConvert.DeserializeObject<List<ReadTerritoryDto>>(response.data.ToString());
+
+
+            return View(categories);
+
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Details(int id)
+        {
+            var response = await _territoryService.GetAsync<ApiResponse>(x => id, HttpContext.Session.GetString(Shared.SessionToken));
+
+            var category = JsonConvert.DeserializeObject<ReadTerritoryDto>(response.data.ToString());
+
+            return View(category);
+        }
+
+
+        [HttpGet]
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateTerritoryDto createTerritoryDto)
+        {
+            var response = await _territoryService.CreateAsync<ApiResponse>(createTerritoryDto, HttpContext.Session.GetString(Shared.SessionToken));
+
+            if (response.IsSuccess)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Update(int id)
+        {
+
+            var response = await _territoryService.GetAsync<ApiResponse>(x => id, HttpContext.Session.GetString(Shared.SessionToken));
+
+            var category = JsonConvert.DeserializeObject<UpdateTerritoryDto>(response.data.ToString());
+
+            return View(category);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Update(int id, UpdateTerritoryDto updateTerritoryDto)
+        {
+            var response = await _territoryService.UpdateAsync<ApiResponse>(x => id, updateTerritoryDto, HttpContext.Session.GetString(Shared.SessionToken));
+
+            if (response.IsSuccess)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var response = await _territoryService.GetAsync<ApiResponse>(x => id, HttpContext.Session.GetString(Shared.SessionToken));
+
+            var category = JsonConvert.DeserializeObject<ReadTerritoryDto>(response.data.ToString());
+
+            return View(category);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(ReadCategoryDto model)
+        {
+            var response = await _territoryService.DeleteAsync<ApiResponse>(x => model.Id, HttpContext.Session.GetString(Shared.SessionToken));
+
+            if (response.IsSuccess && response != null)
+            {
+                return Ok("Ok");
+            }
+            else
+            {
+                return BadRequest("Failed");
+            }
+        }
+    }
+}
