11190225bbdfb4858a19ebe3521691374a506428
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-01 00:25:00 +0300

    Add api response model

---
+++ Models/ApiResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace FullWebProjectWithAPI.Web.Models
+{
+    public class ApiResponse
+    {
+        public bool IsSuccess { get; set; } = true;
+        public HttpStatusCode StatusCode { get; set; } = HttpStatusCode.OK;
+        public List<string> ErrorMessages { get; set; }
+        public object data { get; set; }
+    }
+}
