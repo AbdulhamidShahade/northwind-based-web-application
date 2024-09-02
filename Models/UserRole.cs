ee6631e422905d2ea46dfa8e78dc3ee7616e0870
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-02 22:33:44 +0300

    Add UserRole.cs

---
+++ Models/UserRole.cs
@@ -0,0 +1,10 @@
+﻿namespace NorthwindBasedWebApplication.Models
+{
+    public class UserRole
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public bool HasRole { get; set; }
+    }
+}
