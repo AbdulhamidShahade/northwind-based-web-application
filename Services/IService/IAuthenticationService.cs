7e14cbcdd794661d1e7a6b305150f7ec35e80f8d
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-02 22:34:17 +0300

    Add IAuthenticationService.cs

---
+++ Services/IService/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using NorthwindBasedWebApplication.Models.Dtos.AuthenticationDtos;
+
+namespace NorthwindBasedWebApplication.Services.IService
+{
+    public interface IAuthenticationService
+    {
+        Task<T> Login<T>(LoginRequestDto loginRequestDto);
+        Task<T> Register<T>(RegisterRequestDto registerRequestDto);
+    }
+}
