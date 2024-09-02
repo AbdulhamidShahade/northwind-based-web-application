f0904ff9461eed9c7647f7c171e8708167911d40
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-02 22:33:36 +0300

    Add GlobalSettings.cs

---
+++ Models/GlobalSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace NorthwindBasedWebApplication.Models
+{
+    public class GlobalSettings
+    {
+        public bool IsAuthenticatedUser { get; set; } = false;
+        public string Email { get; set; }
+
+
+
+    }
+}
