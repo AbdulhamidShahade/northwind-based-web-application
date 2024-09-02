417b7fbc4e2d5b3d637078e8c47e3293c43d56d3
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-02 22:34:39 +0300

    Add AuthenticationService.cs

---
+++ Services/Service/AuthenticationService.cs
@@ -0,0 +1,42 @@
+﻿using FullWebProjectWithAPI.Web.Services.Base;
+using Microsoft.AspNetCore.Authentication;
+using NorthwindBasedWebApplication.Models;
+using NorthwindBasedWebApplication.Models.Dtos.AuthenticationDtos;
+using NorthwindBasedWebApplication.Services.IService;
+using System.Security.Claims;
+
+namespace NorthwindBasedWebApplication.Services.Service
+{
+    public class AuthenticationService : BaseService, NorthwindBasedWebApplication.Services.IService.IAuthenticationService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly string _url;
+
+        public AuthenticationService(IHttpClientFactory httpClientFactory, IConfiguration configuration) : base(httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            _url = configuration.GetValue<string>("ApiUrls:Base");
+        }
+
+        public async Task<T> Login<T>(LoginRequestDto loginRequestDto)
+        {
+            return await SendAsync<T>(new ApiRequest
+            {
+                ApiType = Models.Enums.ApiType.POST,
+                Data = loginRequestDto,
+                ApiUrl = $"{_url}/Authentication/Login",
+                Token = string.Empty,
+            });
+        }
+        public async Task<T> Register<T>(RegisterRequestDto registerRequestDto)
+        {
+            return await SendAsync<T>(new ApiRequest
+            {
+                ApiType = Models.Enums.ApiType.POST,
+                Data = registerRequestDto,
+                ApiUrl = $"{_url}/Authentication/Register",
+                Token = string.Empty,
+            });
+        }
+    }
+}
