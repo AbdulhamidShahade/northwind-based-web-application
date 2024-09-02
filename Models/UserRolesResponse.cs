feb926410404d6ae6a2cffc66aacdbf84d0860e1
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-02 22:34:03 +0300

    Add UserRolesResponse.cs

---
+++ Models/UserRolesResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace NorthwindBasedWebApplication.Models
+{
+    public class UserRolesResponse
+    {
+        public bool IsSuccess { get; set; } = true;
+        public HttpStatusCode StatusCode { get; set; } = HttpStatusCode.OK;
+        public List<string> ErrorMessages { get; set; }
+        public UserRoles UserRoles { get; set; }
+    }
+}
