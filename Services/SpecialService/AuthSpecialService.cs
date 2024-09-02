41164a3062f0e9662ddffc39876b6abd6755756e
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-02 22:35:04 +0300

    Add AuthSpecialService.cs

---
+++ Services/SpecialService/AuthSpecialService.cs
@@ -0,0 +1,33 @@
+﻿using FullWebProjectWithAPI.Web.Services.Base;
+using NorthwindBasedWebApplication.Models;
+using NorthwindBasedWebApplication.Services.IService;
+
+namespace NorthwindBasedWebApplication.Services.SpecialService
+{
+    public class AuthSpecialService : BaseService
+    {
+        private readonly IAuthorizationService _authorizationService;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AuthSpecialService(IAuthorizationService authorizationService, IHttpClientFactory httpClientFactory) : base(httpClientFactory)
+        {
+            _authorizationService = authorizationService;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<UserRolesResponse> GetRolesByIdAsync(int id)
+        {
+            return _authorizationService.GetRolesById<UserRolesResponse>(id).GetAwaiter().GetResult();
+        }
+
+        public async Task<UserRolesResponse> GetRolesByEmailAsync(string email)
+        {
+            return _authorizationService.GetRolesByEmail<UserRolesResponse>(email).GetAwaiter().GetResult();  
+        }
+
+        public async Task<List<string>> GetRolesNamesByEmailAsync(string email)
+        {
+            return _authorizationService.GetRolesNamesByEmail<List<string>>(email).GetAwaiter().GetResult();
+        }
+    }
+}
