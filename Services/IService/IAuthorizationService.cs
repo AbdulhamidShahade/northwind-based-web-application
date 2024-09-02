d24863cc59d44ef1abdd66049988fbe14e9e9737
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-02 22:34:29 +0300

    Add IAuthorizationService.cs

---
+++ Services/IService/IAuthorizationService.cs
@@ -0,0 +1,11 @@
+﻿using NorthwindBasedWebApplication.Models;
+
+namespace NorthwindBasedWebApplication.Services.IService
+{
+    public interface IAuthorizationService
+    {
+        Task<T> GetRolesById<T>(int id);
+        Task<T> GetRolesByEmail<T>(string email);
+        Task<T> GetRolesNamesByEmail<T>(string email);
+    }
+}
